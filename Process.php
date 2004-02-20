d97ce52ea6a8778d89b79d9991698ef0dee70469
Ian Eure <ieure@php.net>
2004-02-20 00:23:22 +0000

    Split functions intended to help Processors off into Payment_Process_Common to reduce clutter.
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process/trunk@151858 c90b9560-bf6c-de11-be94-00142212c4b1

--- Process.php
+++ Process.php
@@ -15,7 +15,6 @@
 // +----------------------------------------------------------------------+
 // | Authors: Ian Eure <ieure@php.net>                                    |
 // |          Joe Stump <joe@joestump.net>                                |
-// |          Ondrej Jombik <nepto@pobox.sk>                              |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -258,55 +257,6 @@ class Payment_Process extends PEAR {
     }
 
     /**
-     * Validate data before processing.
-     *
-     * This function may be overloaded by the processor.
-     *
-     * @return boolean true if validation succeeded, PEAR_Error if it failed.
-     */
-    function validate()
-    {
-        foreach ($this->getFields() as $field) {
-            $func = '_validate'.ucfirst($field);
-            
-            // Don't validate unset optional fields
-            if (! $this->isRequired($field) && !strlen($this->field)) {
-                continue;
-            }
-            
-            if (method_exists($this, $func)) {
-                $res = $this->$func();
-                if (PEAR::isError($res) || (is_bool($res) && $res == false)) {
-                    if (!$res)
-                        $res = new PEAR_Error("Validation of field \"{$field}\" failed.", PAYMENT_PROCESS_ERROR_INVAILD);
-                    return $res;
-                }
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Process the transaction.
-     *
-     * This function should be overloaded by the processor.
-     */
-    function process()
-    {
-        return PEAR::raiseError("process() is not implemented in this processor.", PAYMENT_PROCESS_ERROR_NOTIMPLEMENTED);
-    }
-
-    /**
-     * Get transaction result.
-     *
-     * This function should be overloaded by the processor.
-     */
-    function getResult()
-    {
-        return PEAR::raiseError("getResult() is not implemented in this processor.", PAYMENT_PROCESS_ERROR_NOTIMPLEMENTED);
-    }
-
-    /**
      * Set multiple fields at once.
      *
      * This function takes a variable number of arguments, in alternating
@@ -485,92 +435,7 @@ class Payment_Process extends PEAR {
     }
 
 
-    /**
-     * Validate a credit card number.
-     *
-     * @access private
-     * @return boolean true on success, false on failure.
-     */
-    function _validateCardNumber()
-    {
-        return Validate::creditCard($this->cardNumber);
-    }
 
-    /**
-     * Validate an email address.
-     *
-     * @access private
-     * @return boolean true on success, false on failure.
-     */
-    function _validateEmail()
-    {
-        if (isset($this->email) && strlen($this->email)) {
-            return Validate::email($this->email, false);
-        }
-        return true;
-    }
-
-    /**
-     * Validate transaction type.
-     *
-     * @access private
-     * @return boolean true on success, false on failure.
-     */
-    function _validateType()
-    {
-        return $this->_isDefinedConst($this->type, 'type');
-    }
-
-    /**
-     * Validate transaction acion.
-     *
-     * @access private
-     * @return boolean true on success, false on failure.
-     */
-    function _validateAction()
-    {
-        return (isset($GLOBALS['_'.$this->_driver][$this->type]));
-    }
-
-    /**
-     * Validate transaction source.
-     *
-     * @access private
-     * @return boolean true on success, false on failure.
-     */
-    function _validateSource()
-    {
-        return $this->_isDefinedConst($this->transactionSource, 'source');
-    }
-    
-    /**
-     * Validate the charge amount.
-     *
-     * Charge amount must be 8 characters long, double-precision.
-     * Current min/max are rather arbitrarily set to $1.00 and $99999.99,
-     * respectively.
-     *
-     * @return boolean true on success, false otherwise
-     */
-    function _validateAmount()
-    {
-        return Validate::number($this->amount, array(
-            'decimal' => '.',
-            'dec_prec' => 2,
-            'min' => 1.00,
-            'max' => 99999.99
-        ));
-    }
-
-    /**
-     * Validate the zip code.
-     *
-     * @return boolean true on success, false otherwise
-     */
-    function _validateZip()
-    {
-        return ereg('^[0-9]{5}(-[0-9]{4})?$', $this->zip);
-    }
 
     /**
      * See if a value is a defined constant.
@@ -594,56 +459,6 @@ class Payment_Process extends PEAR {
             }
         }
         return @in_array($value, $valid);
-    }
-
-    /**
-     * Prepare the POST data.
-     *
-     * This function handles translating the data set in the front-end to the
-     * format needed by the back-end. The prepared data is stored in 
-     * $this->_data. If a '_handleField' method exists in this class (e.g. 
-     * '_handleCardNumber()'), that function is called and /must/ set 
-     * $this->_data correctly. If no field-handler function exists, the data 
-     * from the front-end is mapped into $_data using $this->_fieldMap.
-     *
-     * @access private
-     * @return array Data to POST
-     */
-    function _prepare()
-    {
-        $this->_data = array();
-        foreach ($this->_fieldMap as $generic => $specific) {
-            $func = '_handle'.ucfirst($generic);
-            if (method_exists($this, $func)) {
-                $result = $this->$func();
-                if (PEAR::isError($result)) {
-                    return $result;
-                }
-            } else {
-                $this->_data[$specific] = $this->$generic;
-            }
-        }
-
-        if ($this->_options['testTransaction']) {
-            $this->_data['testTransaction'] = $this->_options['testTransaction'];
-        }
-                                                                                
-        return true;
-    }
-
-    /**
-     * Handle action
-     *
-     * Actions are defined in $GLOBALS['_Payment_Process_DriverName'] and then
-     * handled here. We may decide to abstract the defines in the driver.
-     *
-     * @author Joe Stump <joe@joestump.net>
-     * @access private
-     * @return void
-     */
-    function _handleAction()
-    {
-        $this->_data[$this->_fieldMap['action']] = $GLOBALS['_'.$this->_driver][$this->action];
     }
 
     /**
