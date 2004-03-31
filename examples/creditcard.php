bc4e5067c8748c3359d7f83264e5d688e190dde7
Joe Stump <jstump@php.net>
2004-03-31 19:28:40 +0000

    - Added new credit card example (not new, just moved)
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process/trunk@155084 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/creditcard.php
@@ -0,0 +1,63 @@
+<?php
+
+  require_once('Payment/Process.php');
+
+  $options = array();
+  $options['x_test_request'] = 'TRUE';
+  $options['x_delim_data'] = 'TRUE';
+  $options['avsCheck'] = true;
+  $options['cvvCheck'] = true;
+
+  $process = & Payment_Process::factory('AuthorizeNet',$options);
+  if (!PEAR::isError($process)) {
+      $process->_debug = true;
+      $process->login = 'username';
+      $process->password = 'password';
+      $process->action = PAYMENT_PROCESS_ACTION_AUTHONLY;
+      $process->amount = 1.00;
+
+      $card = & Payment_Process_Type::factory(PAYMENT_PROCESS_TYPE_CREDITCARD);
+      if (!PEAR::isError($card)) {
+          $card->type = PAYMENT_PROCESS_CC_VISA;
+          $card->invoiceNumber = 112345145;
+          $card->customerId = 1461264151;
+          $card->cardNumber = '4111111111111111';
+          $card->expDate = '01/2005';
+          $card->zip = '48197';
+          $card->cvv = '768';
+
+          if (Payment_Process_Type::isValid($card)) {
+              if(!$process->setPayment($card)) {
+                  die("Unable to set payment\n");
+              }
+
+              $result = $process->process();
+              if (PEAR::isError($result)) {
+                  echo "\n\n";
+                  echo $result->getMessage()."\n";
+              } else {
+                  print_r($result);
+                  echo "\n";
+                  echo "---------------------- RESPONSE ------------------------\n";
+                  echo $result->getMessage()."\n";
+                  echo $result->getCode()."\n";
+                  $validate = $result->validate();
+                  if(!PEAR::isError($validate)) {
+                      echo "All good\n";
+                  } else {
+                      echo "ERROR: ".$validate->getMessage()."\n";
+                  }
+    
+                  echo "---------------------- RESPONSE ------------------------\n";
+              }
+          } else {
+              echo 'Something is wrong with your card!'."\n";
+          }
+      } else {
+        echo $card->getMessage()."\n";
+      }
+  } else {
+      echo $payment->getMessage()."\n";
+  }
+
+?>
