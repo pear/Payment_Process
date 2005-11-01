1a6fc89edb586f6f72a969723f2a3551413e57bc
Philippe Jausions <jausions@php.net>
2005-11-01 19:15:59 +0000

    Updated docblock comments
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process/trunk@199726 c90b9560-bf6c-de11-be94-00142212c4b1

--- Process/Type/CreditCard.php
+++ Process/Type/CreditCard.php
@@ -1,23 +1,29 @@
 <?php
+
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at                              |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Ian Eure <ieure@php.net>                                    |
-// |          Joe Stump <joe@joestump.net>                                |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+
+/**
+ * Represents a credit card type of payment
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Payment
+ * @package    Payment_Process
+ * @author     Ian Eure <ieure@php.net>
+ * @author     Joe Stump <joe@joestump.net>
+ * @author     Philippe Jausions <Philippe.Jausions@11abacus.com>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Payment_Process
+ * @see        Validate_Finance_CreditCard
+ */
 
 /**
  * Payment_Process_Type_CreditCard
@@ -25,18 +31,49 @@
  * @author Joe Stump <joe@joestump.net>
  * @package Payment_Process
  */
-class Payment_Process_Type_CreditCard extends Payment_Process_Type 
+class Payment_Process_Type_CreditCard extends Payment_Process_Type
 {
+    /**
+     * Self-identifying payment type
+     *
+     * @var string $_type
+     */
     var $_type = 'CreditCard';
-    var $cardNumber;
+
+    /**
+     * Credit card type
+     *
+     * @var int $type one of PAYMENT_PROCESS_CC_* constant
+     */
     var $type;
+
+    /**
+     * Credit card number
+     *
+     * @var string $cardNumber
+     */
+    var $cardNumber;
+
+    /**
+     * Card Verification Value
+     *
+     * a.k.a CVV2, CVC, CID
+     *
+     * @var int $cvv
+     */
     var $cvv;
+
+    /**
+     * Card expiry date
+     *
+     * @var string $expDate expiry date in MM/YYYY format
+     */
     var $expDate;
 
     function __construct()
     {
+        require_once 'Validate/Finance/CreditCard.php';
         parent::__construct();
-
     }
 
     function Payment_Process_Type_CreditCard()
@@ -50,7 +87,7 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
      * Uses Validate_Finance_CreditCard to validate the card number.
      *
      * @author Joe Stump <joe@joestump.net>
-     * @return mixed PEAR_Error on failure, true on success
+     * @return mixed PEAR_Error on failure, TRUE on success
      * @see Payment_Process_Type_CreditCard::_getValidateTypeMap()
      * @see Validate_Finance_CreditCard
      */
@@ -64,12 +101,12 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
     }
 
     /**
-     * _validateType
+     * Validates the credit card type
      *
      * Uses Validate_Finance_CreditCard to validate the type.
      *
      * @author Joe Stump <joe@joestump.net>
-     * @return mixed PEAR_Error on failure, true on success
+     * @return mixed PEAR_Error on failure, TRUE on success
      * @see Payment_Process_Type_CreditCard::_getValidateTypeMap()
      * @see Validate_Finance_CreditCard
      */
@@ -112,9 +149,10 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
     /**
      * Validate the card's expiration date.
      *
-     * @todo Fix YxK issues; an expyear of '99' will come up as valid.
-     * @author Joe Stump <joe@joestump.net>
      * @return boolean true on success, false otherwise
+     * @access protected
+     * @author Joe Stump <joe@joestump.net>
+     * @todo Fix YxK issues; an expyear of '99' will come up as valid.
      */
     function _validateExpDate()
     {
@@ -122,13 +160,12 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
         if (!is_numeric($month) || !is_numeric($year)) {
             return PEAR::raiseError('Invalid expiration date provided');
         }
-        
+
         $monthOptions = array('min'     => 1,
                               'max'     => 12,
                               'decimal' => false);
-
         $date = getdate();
-                                                                                
+
         $yearOptions  = array('min'     => $date['year'],
                               'decimal' => false);
 
@@ -139,7 +176,7 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
                 return true;
             }
         }
-                                                                                
+    
         return PEAR::raiseError('Invalid expiration date provided');
     }
 
@@ -147,30 +184,30 @@ class Payment_Process_Type_CreditCard extends Payment_Process_Type
      * Maps a PAYMENT_PROCESS_CC_* constant with a with a value suitable
      * to Validate_Finance_CreditCard package
      *
-     * @return string card type name
+     * @return string|boolean card type name or FALSE on error
      * @access private
      */
     function _mapType()
     {
         switch ($this->type) {
-        case PAYMENT_PROCESS_CC_MASTERCARD:
-            return 'MasterCard';
-        case PAYMENT_PROCESS_CC_VISA:
-            return 'Visa';
-        case PAYMENT_PROCESS_CC_AMEX:
-            return 'Amex';
-        case PAYMENT_PROCESS_CC_DISCOVER:
-            return 'Discover';
-        case PAYMENT_PROCESS_CC_JCB:
-            return 'JCB';
-        case PAYMENT_PROCESS_CC_DINERS:
-            return 'Diners';
-        case PAYMENT_PROCESS_CC_ENROUTE:
-            return 'EnRoute';
-        case PAYMENT_PROCESS_CC_CARTEBLANCHE:
-            return 'CarteBlanche';
-        default:
-            return false;
+            case PAYMENT_PROCESS_CC_MASTERCARD:
+                return 'MasterCard';
+            case PAYMENT_PROCESS_CC_VISA:
+                return 'Visa';
+            case PAYMENT_PROCESS_CC_AMEX:
+                return 'Amex';
+            case PAYMENT_PROCESS_CC_DISCOVER:
+                return 'Discover';
+            case PAYMENT_PROCESS_CC_JCB:
+                return 'JCB';
+            case PAYMENT_PROCESS_CC_DINERS:
+                return 'Diners';
+            case PAYMENT_PROCESS_CC_ENROUTE:
+                return 'EnRoute';
+            case PAYMENT_PROCESS_CC_CARTEBLANCHE:
+                return 'CarteBlanche';
+            default:
+                return false;
         }
     }
 }
