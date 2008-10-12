b1222ed2825f2c771f72b7cc69428ce0fdf30f48
Philippe Jausions <jausions@php.net>
2008-10-12 19:37:31 +0000

    - Fixes bug #14439 (Added way to set CURL options to AuthorizeNet)
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process/trunk@267241 c90b9560-bf6c-de11-be94-00142212c4b1

--- Process/AuthorizeNet.php
+++ Process/AuthorizeNet.php
@@ -8,31 +8,31 @@
  * PHP versions 4 and 5
  *
  * LICENSE:
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, this 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation 
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * 3. The name of the authors may not be used to endorse or promote products 
+ * 3. The name of the authors may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR IMPLIED 
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
- * EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * @category  Payment
  * @package   Payment_Process
  * @author    Joe Stump <joe@joestump.net>                                |
@@ -228,10 +228,25 @@ class Payment_Process_AuthorizeNet extends Payment_Process_Common
             PEAR::popErrorHandling();
             return $curl;
         }
+        $result = $curl->create();
+        if (PEAR::isError($result)) {
+            PEAR::popErrorHandling();
+            return $result;
+        }
 
         $curl->type = 'post';
         $curl->fields = $fields;
-        $curl->userAgent = 'PEAR Payment_Process_AuthorizeNet 0.1';
+        $curl->userAgent = 'PEAR Payment_Process_AuthorizeNet @package_version@';
+
+        if (isset($this->_options['curl_options'])) {
+            foreach ($this->_options['curl_options'] as $key => $value) {
+                if (!$curl->setOption($key, $value)) {
+                    PEAR::popErrorHandling();
+                    return PEAR::raiseError("Curl option ($key = $value) failed");
+                }
+            }
+        }
+
 
         $result = $curl->execute();
         if (PEAR::isError($result)) {
